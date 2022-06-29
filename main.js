8be68c7963b922721ea02fd8545115501958fb4a
FrancoOrellana1 <99762479+FrancoOrellana1@users.noreply.github.com>
2022-06-29 16:28:39 -0300

    Se agregan funciones
    Se realiza para facilitar su lectura

--- main.js
+++ main.js
@@ -8,53 +8,81 @@ const $agregar = document.querySelector('#agregar');
 let numeroIntegrante = 1;
 
 $agregar.onclick= function(){
+    
+    agregarElementos();
+
+    const $calcular = document.querySelector('#calcular');
+
+    $calcular.onclick = function (){
+        const $salario = document.querySelectorAll('.salario');
+        const arraySalarios = [];
+
+        for(let i=0; i<$salario.length; i++){
+            if($salario[i].value !== ''){
+                arraySalarios.push($salario[i].value)
+            }
+        
+        }
+
+        mayorSalario(arraySalarios);
+        menorSalario(arraySalarios);
+        promedios(arraySalarios);
+
+
+        return false
+    }
+    return false
+}
+
+
+function agregarElementos(){
     const nuevoInput = document.querySelector('form');
     const cuerpoNuevoInput = document.createElement('div');
-    cuerpoNuevoInput.innerHTML= ('<input type="number" id="salario">');
+    cuerpoNuevoInput.innerHTML= ('<input type="number" class="salario">');
     const nuevoParrafo = document.querySelector('form');
     const textoNuevoParrafo = document.createElement('p');
     textoNuevoParrafo.innerText = ('Integrante N°' + numeroIntegrante);
     nuevoParrafo.appendChild(textoNuevoParrafo);
     nuevoInput.appendChild(cuerpoNuevoInput);
     numeroIntegrante ++;
-    
 
-    const $calcular = document.querySelector('#calcular');
+    return false;
+}
 
-    $calcular.onclick = function (){
-        const $salario = document.querySelectorAll('#salario');
-        let mayorSalario = 0;
-        for(let i = 0; i< $salario.length; i++){
-            if( mayorSalario < $salario[i].value){
-                mayorSalario = $salario[i].value;
-            }
+function mayorSalario (a){
+    let mayorSalario = 0;
+    for(i = 0; i< a.length; i++){
+        if( mayorSalario < a[i]){
+                mayorSalario = a[i];
         }
-        const $mayor = document.querySelector('#mayor');
-        $mayor.innerText= ('El mayor salario es: ' + mayorSalario);
-
-        let menorSalario = $salario[0].value;
-        for(i=0; i<$salario.length; i++){
-            if(menorSalario>$salario[i].value){
-                menorSalario = $salario[i].value;
-            }
-        
-        }
-        const $menor = document.querySelector('#menor');
-        $menor.innerText= ('El menor salario es: ' + menorSalario);
-
-        let promedioAnual = 0;
-        for(i=0; i<$salario.length; i++){
-            promedioAnual += Number($salario[i].value);
-        }
-        promedioAnual= (promedioAnual/$salario.length);
-        const $promedioAnual = document.querySelector('#promedio-anual');
-        $promedioAnual.innerText= ('El promedio de salario anual es de: '+ promedioAnual);
-
-        let promedioMensual = (promedioAnual/12);
-        const $promedioMensual = document.querySelector('#promedio-mensual');
-        $promedioMensual.innerText =('El promedio de salario mensual es de: '+ promedioMensual);
-
-        return false
     }
-    return false
+    const $mayor = document.querySelector('#mayor');
+    $mayor.innerText= ('El mayor salario es: ' + mayorSalario);
+}
+
+function menorSalario (a){
+    let menorSalario = a[0];
+    for(i=0; i<a.length; i++){
+        if(menorSalario>a[i]){
+                menorSalario = a[i];
+        }
+        
+    }
+    const $menor = document.querySelector('#menor');
+    $menor.innerText= ('El menor salario es: ' + menorSalario);
+}
+
+function promedios (a){
+    let promedioAnual = 0;
+    for(i=0; i<a.length; i++){
+        promedioAnual += Number(a[i]);
+    }
+    console.log(promedioAnual);
+    promedioAnual= (promedioAnual/a.length);
+    const $promedioAnual = document.querySelector('#promedio-anual');
+    $promedioAnual.innerText= ('El promedio de salario anual es de: '+ promedioAnual);
+
+    let promedioMensual = (promedioAnual/12);
+    const $promedioMensual = document.querySelector('#promedio-mensual');
+    $promedioMensual.innerText =('El promedio de salario mensual es de: '+ promedioMensual);
 }
